280a2eb4ff797bf6d127b4aaef89816d8103b79a
Jean Tubay <jean.tubay.92@gmail.com>
2019-12-02 23:39:03 -0500

    fixed auth

--- controllers/listController.js
+++ controllers/listController.js
@@ -2,10 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/list", (req, res) => {
-    if(!res.user) {
-        return res.redirect("/")
-    }
-    
+  if(!req.user){
+      return res.redirect("/")
+  }  
   res.render("list/index", { user: req.user });
 });
 
