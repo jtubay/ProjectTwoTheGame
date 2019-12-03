072bfeca8d2f9a4df2f42e35cd66802d63e78893
Michael Trpak <mrtrpak3@gmail.com>
2019-12-03 14:23:28 -0500

    used sequelize to create a class table which added a file to migrations and models

---
+++ migrations/20191203191158-create-class.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Classes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      health: {
+        type: Sequelize.INTEGER
+      },
+      minDamage: {
+        type: Sequelize.INTEGER
+      },
+      maxDamage: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Classes');
+  }
+};
