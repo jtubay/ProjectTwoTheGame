682ebb2a276394ae694476779a610039300a2979
Jean Tubay <jean.tubay.92@gmail.com>
2019-12-03 14:12:59 -0500

    added typewriter eff

--- public/assets/js/general.js
+++ public/assets/js/general.js
@@ -9,3 +9,27 @@ var txt = 'Help Defend the Kol-da-sack'; /* The text */
     setTimeout(typeWriter, speed);
   }
 }*/
+
+function changeText(cont1, cont2, speed) {
+  var Otext = cont1.text();
+
+  var Ocontent = Otext.split("");
+
+  var i = 0;
+
+  function show() {
+    if (i < Ocontent.length) {
+      cont2.append(Ocontent[i]);
+
+      i = i + 1;
+    }
+  }
+
+  var Otimer = setInterval(show, speed);
+}
+
+$(document).ready(function() {
+  changeText($(".p1"), $(".p2"), 75); //  150 = the Delay time in milliseconds between strokes.
+
+  clearInterval(Otimer);
+});
