a1f69b4899e0d9da4740216fb75e83411e6c20eb
Jean Tubay <jean.tubay.92@gmail.com>
2019-12-02 23:32:15 -0500

    added rejection if use is not logged in

--- controllers/listController.js
+++ controllers/listController.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/list", (req, res) => {
+    if(!res.user) {
+        return res.redirect("/")
+    }
+    
   res.render("list/index", { user: req.user });
 });
 
