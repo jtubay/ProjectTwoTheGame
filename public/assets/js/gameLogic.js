13f4e89dfacbecb962d3ba32fd4b09d7039e0712
GabrielTMangum <gabrieltmangum@gmail.com>
2019-12-04 13:16:57 -0500

    continued progress on gameLogic.js

--- public/assets/js/gameLogic.js
+++ public/assets/js/gameLogic.js
@@ -1,58 +1,88 @@
+$.get("/api/currentUser", data => {
+    $.get(`/api/class/${data.class}`, currentPlayer => {
+        console.log(currentPlayer)
+        let userHp = currentPlayer.health
 
+        let healthFactor = 100 / userHp
+        
 
-$.ajax("/api/" + id)
+        function Enemy() {
+            this.name = "Enemy",
+                this.health = 100,
+                this.attack = function attack() {
+                    let dmgVal = getRndInt(5, 15);
 
-function Enemy () {
-    this.hp = 100,
-    this.attack = function attack() {
-        let dmgVal = getRndInt(5, 15)
+                    let checkHit = getRndInt(1, 100);
+                    console.log(checkHit);
 
-        let checkHit = getRndInt(1, 100)
-        console.log(checkHit)
-
-        if (checkHit >= 75) {
-            console.log("attack succesful")
-            // dmgPlayer(dmgVal);
-        } else {
-            console.log("you're a failure")
+                    if (checkHit >= 75) {
+                        console.log("attack succesful");
+                        // dmgPlayer(dmgVal);
+                    } else {
+                        console.log("you're a failure");
+                    }
+                }
         }
-    }
-}
 
-const battleStart = () => {
-    const newEnemy = new Enemy();
-    return newEnemy;
-}
+        const generateEnemy = () => {
+            const newEnemy = new Enemy();
+            return newEnemy;
+        }
 
-const getRndInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-const baseAttack = (min, max) => {
-    let dmgVal = getRndInt(min, max)
+        const getRndInt = (min, max) => {
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
+        const baseAttack = (min, max) => {
+            let dmgVal = getRndInt(min, max);
 
-}
+            tookDamage(dmgVal, enemy);
 
-const heavyAttack = (min, max) => {
-    let dmgVal = getRndInt(min, max)
-    
-    let checkHit = getRndInt(1, 100)
+            enemyTurn();
+        }
 
-    if (checkHit >= 75) {
-        dmgEnemy(dmgVal);
-    } else {
-        console.log("you're a failure")
-    }
-}
+        const heavyAttack = (min, max) => {
+            let dmgVal = getRndInt(min, max);
 
+            let checkHit = getRndInt(1, 100);
 
+            if (checkHit >= 75) {
+                tookDamage(dmgVal, enemy);
+            } else {
+                console.log("you're a failure");
+            }
+            enemyTurn();
+        }
 
-const enemyTurn = (damageDealt) => {
-    
-}
+        const potion = (min, max) => {
+            let healVal = getRndInt(min, max);
 
-// Code Test
-const enemy = battleStart()
-console.log(enemy)
-console.log(enemy.hp -= 50)
-console.log(enemy)
-enemy.attack();
+            currentPlayer.health += healVal;
+
+            enemyTurn();
+        }
+
+        const tookDamage = (damageDealt, target) => {
+            target.health -= damageDealt;
+            console.log(`${target.name} took ${damageDealt} damage!`)
+            if (target.hp <= 0) {
+                console.log(`${target.name} has died!`)
+            }
+        }
+
+        const enemyTurn = () => {
+            let dmgVal = getRndInt(0, 20)
+
+            tookDamage(dmgVal, currentPlayer)
+        }
+        // Code Test
+        const enemy = generateEnemy()
+        console.log(enemy)
+        tookDamage(20, enemy)
+        console.log(enemy)
+        tookDamage(20, enemy)
+        tookDamage(15, currentPlayer)
+
+        enemy.attack();
+
+    })
+})
